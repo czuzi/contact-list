2940c21b1fe7ff36334f9cc87ee03b5831ef39e2
czuzi <palocsayi@gmail.com>
2020-05-27 00:03:24 +0200

    changed every components into hooks, besides the ContactList.js

--- src/AddContact.js
+++ src/AddContact.js
@@ -1,31 +1,29 @@
-import React, { Component } from "react"
+import React from "react"
 import "./AddContact.css"
 
-class AddNew extends Component {
-  render() {
-    return (
-      <div className="AddNew container">
-        <h2>Add New Contact</h2>
-        <form>
-          <div className="form-group">
-            <label>Name</label>
-            <input name="name" type="text" className="form-control" id="name" required />
-          </div>
-          <div className="form-group">
-            <label>Email address</label>
-            <input name="email" type="text" className="form-control" id="email" required />
-          </div>
-          <div className="form-group">
-            <label>Phone number</label>
-            <input name="phone" type="text" className="form-control" id="phone" required />
-          </div>
-          <div className="input-button">
-            <input type="submit" className="btn btn-outline-dark" value="Add Contact" />
-          </div>
-        </form>
-      </div>
-    )
-  }
+function AddNew() {
+	return (
+		<div className="AddNew container">
+			<h2>Add New Contact</h2>
+			<form>
+				<div className="form-group">
+					<label>Name</label>
+					<input name="name" type="text" className="form-control" id="name" required />
+				</div>
+				<div className="form-group">
+					<label>Email address</label>
+					<input name="email" type="text" className="form-control" id="email" required />
+				</div>
+				<div className="form-group">
+					<label>Phone number</label>
+					<input name="phone" type="text" className="form-control" id="phone" required />
+				</div>
+				<div className="input-button">
+					<input type="submit" className="btn btn-outline-dark" value="Add Contact" />
+				</div>
+			</form>
+		</div>
+	)
 }
 
 export default AddNew
--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,16 @@
-import React, { Component } from "react"
+import React from "react"
 import { Link } from "react-router-dom"
 import { ReactComponent as Logo } from "./vodafone_logo.svg"
 import "./Header.css"
 
-class Header extends Component {
-  render() {
-    return (
-      <div className="container">
-        <Link to="/">
-          <Logo className="header-logo" />
-        </Link>
-      </div>
-    )
-  }
+function Header() {
+	return (
+		<div className="container">
+			<Link to="/">
+				<Logo className="header-logo" />
+			</Link>
+		</div>
+	)
 }
 
 export default Header

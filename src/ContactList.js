e2fff01a39fb1b92460ab027581c8e410d4e29b3
czuzi <palocsayi@gmail.com>
2020-05-27 02:14:01 +0200

    now theres hooks everywhere

--- src/ContactList.js
+++ src/ContactList.js
@@ -1,50 +1,53 @@
-import React, { Component } from "react"
+import React, { useState } from "react"
 import { Link } from "react-router-dom"
 import Contact from "./Contact"
 import "./ContactList.css"
-import { ContactsData } from "./ContactsData.json"
 
-class ContactList extends Component {
-  constructor() {
-    super()
-    this.state = { search: "" }
-  }
+function ContactList() {
+	const ContactsData = [
+		{ id: 1, name: "Kiss Bence", phone: "+36707070700", email: "bence.kiss@kissbence.hu" },
+		{ id: 2, name: "Kovács Ibolya", phone: "+36707070456", email: "ibolya.kovacs@kovacsibolya.hu" },
+		{ id: 3, name: "Nagy Dávid", phone: "+36707070441", email: "david.nagy@nagydavid.hu" },
+		{ id: 4, name: "Apro Bálint", phone: "+36707070568", email: "balint.apro@aprobalint.hu" },
+		{ id: 5, name: "Czöndör László", phone: "+36707070213", email: "laszlo.czondor@czondorlaszlo.hu" },
+		{ id: 6, name: "Nagy Renáta", phone: "+36707070187", email: "renata.nagy@nagyrenata.hu" },
+		{ id: 7, name: "Dávid Zoltán", phone: "+36707070487", email: "zoltan.david@davidzoltan.hu" },
+		{ id: 8, name: "Tóth Tamás", phone: "+36707070264", email: "tamas.toth@tothtamas.hu" },
+		{ id: 9, name: "Fodor Bernadet", phone: "+36707070264", email: "bernadett.fodor@fodorbernadett.hu" },
+	]
 
-  updateSearch(event) {
-    this.setState({ search: event.target.value.substr(0, 20) })
-  }
+	const [searchTerm, setSearchTerm] = useState("")
+	const handleChange = (e) => {
+		setSearchTerm(e.target.value)
+	}
 
-  render() {
-    let filteredContacts = ContactsData.filter(
-      contact => contact.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1
-    )
+	const filteredContacts = ContactsData.filter((contact) => contact.name.toLowerCase().includes(searchTerm))
 
-    return (
-      <div className="container contact-list">
-        <div className="search">
-          <input
-            className="form-control"
-            type="text"
-            value={this.state.search}
-            onChange={this.updateSearch.bind(this)}
-            placeholder="Search for contract"
-          />
-        </div>
-        <h1>Contacts</h1>
-        <hr />
-        <div className="row row-cols-1 row-cols-md-3">
-          {filteredContacts.map(c => (
-            <div className="col">
-              <Contact key={c.id} id={c.id} name={c.name} phone={c.phone} email={c.email} />
-            </div>
-          ))}
-        </div>
-        <Link to="/addcontact">
-          <div className="plus"></div>
-        </Link>
-      </div>
-    )
-  }
+	return (
+		<div className="container contact-list">
+			<div className="search">
+				<input
+					className="form-control"
+					type="text"
+					value={searchTerm}
+					onChange={handleChange}
+					placeholder="Search for contact"
+				/>
+			</div>
+			<h1>Contacts</h1>
+			<hr />
+			<div className="row row-cols-1 row-cols-md-3">
+				{filteredContacts.map((c) => (
+					<div className="col">
+						<Contact key={c.id} id={c.id} name={c.name} phone={c.phone} email={c.email} />
+					</div>
+				))}
+			</div>
+			<Link to="/addcontact">
+				<div className="plus"></div>
+			</Link>
+		</div>
+	)
 }
 
 export default ContactList
